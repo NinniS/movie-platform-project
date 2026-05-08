de7a9b425d8b26681045bb75675ad3489dcc4d91
Ninni Salomonsson <ninni.salomonsson@gmail.com>
2026-05-08 12:49:07 +0200

    Update users.js

--- assets/JS/users.js
+++ assets/JS/users.js
@@ -1,3 +1,10 @@
 class User{
-    
+    constructor(data) {
+        this.id = data.id;
+        this.userName = data.userName;
+        this.profilePicture = data.profilePicture;
+        this.password = data.password;
+        this.cookie = data.cookie;
+        this.password = data.password;
+  }
 }
