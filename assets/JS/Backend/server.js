0d60d54744d0174d13f9ef76225ce32d372c356e
missWolma <wilma.carlheim@gmail.com>
2026-05-13 15:05:19 +0200

    Update server.js
    I made the beginning of the backend class, have a look and see if it looks alright pls!

--- assets/JS/Backend/server.js
+++ assets/JS/Backend/server.js
@@ -1,5 +1,22 @@
-async function handler(request){
+class BACKEND_SERVER {
+    async handler(request) {
+        //mste man ha en construktor? I tidigare uppgifter använde jag this för att definera olika url, nu la jag allt inut handler funktionen
+        const URL = new URL(request.url);
+        const HEADERS = {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept"
+        }
+        const ACCEPT_HEADER = request.headers.get("accept");
+        const CONTENT_TYPE_HEADER = request.headers.get("Content-Type");
+        const MOVIE_ID = new URLPattern({ pathname: "/movies/:id" });
 
+
+        if(URL == "/movies"){
+            //om url är movies, ska vi ha detta som bas url? Här får man alla filmer
+        }
+    }
 }
 
-Deno.serve
+const BACKEND = new BACKEND_SERVER();
