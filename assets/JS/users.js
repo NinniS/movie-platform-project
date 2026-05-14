34b5d07ead676654984c52bf597f990217d61e30
Ninni Salomonsson <ninni.salomonsson@gmail.com>
2026-05-14 20:26:02 +0200

    Added comment

--- assets/JS/users.js
+++ assets/JS/users.js
@@ -1,3 +1,7 @@
+// Jag tog inspiration från lektioner/uppgifter som vi
+// hade med employees och departments, där vi hade en register
+// konstant som vi sparade alla departments och employees i.
+
 export class User{
     static getAllUsers(){
 
@@ -33,8 +37,12 @@ const db = {
         const users = [];
         for (let oneUser of data) {
             const userInstance = new User(oneUser);
-            users.push(userInstance);
+            db.users.push(userInstance);
         }
         return users;
     }
 }
+
+db.initUsers();
+
+//Deno.serve(handler);
