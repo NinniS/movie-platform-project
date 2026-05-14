aaee259fd39c8d365c8608fac09748c93996a2a5
missWolma <wilma.carlheim@gmail.com>
2026-05-14 19:21:46 +0200

    Update server.js

--- assets/JS/Backend/server.js
+++ assets/JS/Backend/server.js
@@ -1,37 +1,43 @@
-import {} from
+import { MOVIES } from "./movies.js";
 
-    async function handler(request) {
-        const URL = new URL(request.url);
-        const HEADERS = {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept"
-        }
-        const ACCEPT_HEADER = request.headers.get("accept");
-        const CONTENT_TYPE_HEADER = request.headers.get("Content-Type");
-        const MOVIE_ID_PATTERN = new URLPattern({ pathname: "/movies/:id" });
+async function handler(request) {
+    const URL = new URL(request.url);
+    const HEADERS = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept"
+    }
+    const ACCEPT_HEADER = request.headers.get("accept");
+    const CONTENT_TYPE_HEADER = request.headers.get("Content-Type");
+    const MOVIE_ID_PATTERN = new URLPattern({ pathname: "/movies/:id" });
+
+    if (request.method === "OPTIONS") {
+        return new Response(null, {
+            status: 204,
+            headers: HEADERS
+        });
+    }
 
 
-        if (URL.pathname == "/movies") {
-            //om url är movies, ska vi ha detta som bas url? Här får man alla filmer
-        }
+    if (URL.pathname == "/movies") {
+        //om url är movies, ska vi ha detta som bas url? Här får man alla filmer
+    }
 
-        if (URL.pathname == "/movies/genres") {
-            //kod om att ta ut alla genres
-        }
+    if (URL.pathname == "/movies/genres") {
+        //kod om att ta ut alla genres
+    }
 
-        if (url.pathname == "/movies/search") {
-            let searchQuery = url.searchParams.get("q");
-            //Vill vi ha liknade koller efter felkoder som i U?
-            if (!searchQuery) {
-                return new Response({ error: "Bad Request" }, {
-                    status: 400,
-                    headers: HEADERS
-                });
-            }
-
+    if (url.pathname == "/movies/search") {
+        let searchQuery = url.searchParams.get("q");
+        //Vill vi ha liknade koller efter felkoder som i U?
+        if (!searchQuery) {
+            return new Response({ error: "Bad Request" }, {
+                status: 400,
+                headers: HEADERS
+            });
         }
 
     }
-    }
+
+}
