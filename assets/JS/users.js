9d1ef08711cc1015b9eae6f4903a890968549189
Ninni Salomonsson <ninni.salomonsson@gmail.com>
2026-05-14 19:55:59 +0200

    added a bit of things to classes user and review, plus const db

--- assets/JS/users.js
+++ assets/JS/users.js
@@ -1,10 +1,40 @@
-class User{
+export class User{
+    static getAllUsers(){
+
+    }
+    
     constructor(data) {
         this.id = data.id;
         this.userName = data.userName;
         this.profilePicture = data.profilePicture;
-        this.password = data.password;
         this.cookie = data.cookie;
         this.password = data.password;
   }
+}
+
+export class Review{
+    constructor(data) {
+        this.id = data.id;
+        this.movieId = data.movieId;
+        this.score = data.score;
+        this.reviewText = data.reviewText;
+    }
+    
+    static getAllReviewsByMovieId(id){
+
+    }
+}
+
+const db = {
+    users: [],
+    reviews: [],
+    initUsers(){
+        const data = JSON.parse(Deno.readTextFileSync("./database/users.json"));
+        const users = [];
+        for (let oneUser of data) {
+            const userInstance = new User(oneUser);
+            users.push(userInstance);
+        }
+        return users;
+    }
 }
