23fda317b25300db301e0d99157fe4dd78409119
mendytran <mendy.tran.04@gmail.com>
2026-05-18 17:27:10 +0200

    added ui and api functions

--- assets/JS/ui.js
+++ assets/JS/ui.js
@@ -1,3 +1,28 @@
-class UI_CLASS{
-    
+import { API } from "./api.js";
+
+class UI_CLASS {
+    async fillAllMovies() {
+        const movieSection = document.querySelector("#all-movies");
+        try {
+            const movies = await API.getResource("/movies")
+
+            for (let movie of movies) {
+                let divDM = document.createElement("div");
+                divDM.innerHTML = `
+            <div>
+            <img src="${movie.imageURL}">
+            <h2>${movie.title}</h2>
+            </div>
+            `;
+                divDM.classList.add("movie");
+                movieSection.appendChild(divDM);
+            }
+        } catch (error) {
+            console.log("Didn't work");
+            return;
+        }
+    }
 }
+
+const UI = new UI_CLASS;
+UI.fillAllMovies();
