0deff8b73ff56fcbf7c57cce892f3fd599c8dbbe
missWolma <wilma.carlheim@gmail.com>
2026-05-20 16:03:48 +0200

    Added user.js functions to use in server.js

--- assets/JS/Backend/server.js
+++ assets/JS/Backend/server.js
@@ -1,7 +1,6 @@
 import { serveDir, serveFile } from "jsr:@std/http/file-server";
 import { MOVIES } from "./movies.js";
-import { USERS } from "./users.js";
-import { REVIEWS } from "./users.js";
+import { USERS, REVIEWS } from "./users.js";
 
 const HEADERS = {
     "Content-Type": "application/json",
@@ -10,7 +9,6 @@ const HEADERS = {
     "Access-Control-Allow-Headers": "Content-Type, Authorization"
 }
 
-//SE MIG!!!!!
 const cookie = [];
 
 function makeResponse(type) {
@@ -53,7 +51,11 @@ async function handler(request) {
     const url = new URL(request.url);
     const ACCEPT_HEADER = request.headers.get("accept");
     const CONTENT_TYPE_HEADER = request.headers.get("Content-Type");
+    const REVIEW_BY_MOVIE_ID_PATTERN = new URLPattern({ pathname: "/movies/reviews/:id" });
     const MOVIE_ID_PATTERN = new URLPattern({ pathname: "/movies/:id" });
+    const REVIEW_BY_USER_ID_PATTERN = new URLPattern({ pathname: "/user/reviews/:id" });
+    const WATCHLIST_BY_ID_PATTERN = new URLPattern({ pathname: "/user/watchlist/:id" });
+
 
     if (request.method === "OPTIONS") {
         return new Response(null, {
@@ -66,24 +68,24 @@ async function handler(request) {
         return serveFile(request, "../../../frontend/homepage.html");
     }
 
-    if(url.pathname == "/login"){
-        if(request.method == "GET"){
+    if (url.pathname == "/login") {
+        if (request.method == "GET") {
             return serveFile(request, "../../../frontend/log-in.html");
         }
-        if(request.method == "POST"){
+        if (request.method == "POST") {
             // console.log("recived login");
             let loginUser = await request.json();
             let allUsers = USERS.getAllUsers();
             // console.log("trying to login with", loginUser);
             // console.log("what are you:", typeof loginUser);
             // console.log("we have", allUsers.length, "users");
-            
-            for(let oneUser of allUsers){
+
+            for (let oneUser of allUsers) {
                 // console.log("is this you", oneUser);
-                if(oneUser.username == loginUser.username && oneUser.password == loginUser.password){
+                if (oneUser.username == loginUser.username && oneUser.password == loginUser.password) {
                     HEADERS["Set-Cookie"] = "session_id=secret-value; Max-Age=84600";
                     // console.log("found user");
-                    return new Response(JSON.stringify({"welcome": "Welcome!"}), {headers: HEADERS});
+                    return new Response(JSON.stringify({ "welcome": "Welcome!" }), { headers: HEADERS });
                 }
             }
             // console.log("could not find user");
@@ -129,6 +131,21 @@ async function handler(request) {
         return new Response(JSON.stringify(foundMovies), { headers: HEADERS });
     }
 
+    if(REVIEW_BY_MOVIE_ID_PATTERN.test(url)){
+        let match = REVIEW_BY_MOVIE_ID_PATTERN.exec(url);
+        let id = match.pathname.groups.id;
+
+        if(request.method == "GET"){
+
+            let reviewByMovieId = REVIEWS.getAllReviewsByMovieId(id);
+
+            if(reviewByMovieId.length == 0){
+                return makeResponse("not found");
+            }
+            return new Response(JSON.stringify(reviewByMovieId), {headers: HEADERS});
+        }
+    }
+
     if (MOVIE_ID_PATTERN.test(url)) {
         let match = MOVIE_ID_PATTERN.exec(url);
         let id = match.pathname.groups.id;
@@ -143,6 +160,36 @@ async function handler(request) {
             return new Response(JSON.stringify(movieById), { headers: HEADERS });
         }
     }
+
+    if (REVIEW_BY_USER_ID_PATTERN.test(url)) {
+        let match = REVIEW_BY_USER_ID_PATTERN.exec(url);
+        let id = match.pathname.groups.id;
+
+        if (request.method == "GET") {
+            let reviewsByUserId = REVIEWS.getReviewsByUserId(id);
+
+            if(reviewsByUserId.length == 0){
+                return makeResponse("not found");
+            }
+            return new Response(JSON.stringify(reviewsByUserId), {headers: HEADERS});
+        }
+    }
+
+    if(WATCHLIST_BY_ID_PATTERN.test(url)){
+        let match = WATCHLIST_BY_ID_PATTERN.exec(url);
+        let id = match.pathname.groups.id;
+
+        if(request.method == "GET"){
+            let watchlistById = USERS.getWatchlistMoviesByUserId(id);
+
+            if(!watchlistById){
+                return makeResponse("not found");
+            }
+            return new Response(JSON.stringify(watchlistById), {headers: HEADERS});
+        }
+    }
+
+
     return serveDir(request, { fsRoot: "../../../" });
 
 }
