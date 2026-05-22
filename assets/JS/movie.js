7eb57a595efb6d76c1f561bea28b01a81c0d73d2
mendytran <mendy.tran.04@gmail.com>
2026-05-22 17:09:32 +0200

    Fixed movie-page appearance

--- assets/JS/movie.js
+++ assets/JS/movie.js
@@ -15,15 +15,32 @@ async function renderMovie(id) {
     let movieDiv = document.createElement("div");
     movieDiv.setAttribute("id", "movie-info")
     movieDiv.innerHTML = `
-        <img style="height: 500px" src="${movie.imageURL}">
-        
-        <div>
+        <div id="movie-section">
+            <img style="height: 500px" src="${movie.imageURL}">
             <div>
-                <p>${movie.title}</p>
-                <p>${movie.year}</p>
+                <div style="border-bottom: 1px solid var(--main-yellow-color)">
+                    <h2>${movie.title}</h2>
+                    <h2>${movie.year}</h2>
+                </div>
+                <div style="border-bottom: 1px solid var(--main-yellow-color)">
+                    <p id="movie-description">${movie.description}</p>
+                </div>
+                <div>
+                    <div id="all-genres"></div>
+                </div>
             </div>
-            <p>${movie.description}</p>
         </div>
     `;
     parentDiv.appendChild(movieDiv);
+    getGenres(movie.genre);
+}
+
+function getGenres(genres) {
+    const parentDiv = document.querySelector("#all-genres");
+    for (let genre of genres) {
+        let pDM = document.createElement("p");
+        pDM.classList.add("genres");
+        pDM.textContent = genre;
+        parentDiv.appendChild(pDM);
+    }
 }
